c79aefdaf4cf5c022e11fffb314c0b6bd3932c76
Youngseo Choi <ys550499@gmail.com>
2023-01-23 23:18:58 +0900

    read

--- src/main/java/example/armeria/blog/grpc/BlogService.java
+++ src/main/java/example/armeria/blog/grpc/BlogService.java
@@ -1,13 +1,20 @@
 package example.armeria.blog.grpc;
 
+import com.linecorp.armeria.internal.shaded.fastutil.objects.Object2LongMap;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Instant;
+import java.util.Map.Entry;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public final class BlogService extends BlogServiceGrpc.BlogServiceImplBase {
     private static final Logger logger = LoggerFactory.getLogger(BlogService.class);
@@ -28,6 +35,35 @@ public final class BlogService extends BlogServiceGrpc.BlogServiceImplBase {
         blogPosts.put(id, updated);
         logger.info("Created at {} - {}", updated.getId(), updated.getTitle());
         responseObserver.onNext(updated);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getBlogPost(GetBlogPostRequest request, StreamObserver<BlogPost> responseObserver) {
+        final BlogPost blogPost = blogPosts.get(request.getId());
+        if (blogPost == null) {
+            responseObserver.onError(
+                    Status.NOT_FOUND.withDescription("The blog post does not exist. ID: " + request.getId())
+                            .asRuntimeException()
+            );
+        } else {
+            responseObserver.onNext(blogPost);
+            responseObserver.onCompleted();
+        }
+    }
+
+    @Override
+    public void listBlogPosts(ListBlogPostsRequest request, StreamObserver<ListBlogPostsResponse> responseObserver) {
+        final Collection<BlogPost> blogPosts;
+        if (request.getDescending()) {
+            blogPosts = this.blogPosts.entrySet()
+                    .stream()
+                    .sorted(Collections.reverseOrder(Comparator.comparingInt(Entry::getKey)))
+                    .map(Entry::getValue).collect(Collectors.toList());
+        } else {
+            blogPosts = this.blogPosts.values();
+        }
+        responseObserver.onNext(ListBlogPostsResponse.newBuilder().addAllBlogs(blogPosts).build());
         responseObserver.onCompleted();
     }
 }
--- src/main/java/example/armeria/client/blog/grpc/BlogClient.java
+++ src/main/java/example/armeria/client/blog/grpc/BlogClient.java
@@ -1,11 +1,12 @@
 package example.armeria.client.blog.grpc;
 
 import com.linecorp.armeria.client.grpc.GrpcClients;
-import example.armeria.blog.grpc.BlogPost;
-import example.armeria.blog.grpc.BlogServiceGrpc;
-import example.armeria.blog.grpc.CreateBlogPostRequest;
+import com.linecorp.armeria.client.logging.LoggingClient;
+import example.armeria.blog.grpc.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public final class BlogClient {
     private static final Logger logger = LoggerFactory.getLogger(BlogClient.class);
@@ -20,12 +21,26 @@ public final class BlogClient {
         logger.info("[Create response] Title: {} Content: {}", response.getTitle(), response.getContent());
     }
 
+    static void getBlogPost(int id) {
+        final BlogPost blogPost = client.getBlogPost(GetBlogPostRequest.newBuilder().setId(id).build());
+    }
+
+    static void listBlogPosts() {
+        final ListBlogPostsResponse response = client.listBlogPosts(ListBlogPostsRequest.newBuilder().setDescending(false).build());
+        final List<BlogPost> blogs = response.getBlogsList();
+    }
+
     void testRun(){
         createBlogPost("Another blog post", "Creating a post via createBlogPost().");
+//        listBlogPosts();
+        getBlogPost(100);
     }
 
     public static void main(String[] args) throws Exception {
-        client = GrpcClients.newClient("http://127.0.0.1:8080/", BlogServiceGrpc.BlogServiceBlockingStub.class);
+//        client = GrpcClients.newClient("http://127.0.0.1:8080/", BlogServiceGrpc.BlogServiceBlockingStub.class);
+        client = GrpcClients.builder("http://127.0.0.1:8080/")
+                .decorator(LoggingClient.newDecorator())
+                .build(BlogServiceGrpc.BlogServiceBlockingStub.class);
         CreateBlogPostRequest request = CreateBlogPostRequest.newBuilder()
                 .setTitle("My first blog")
                 .setContent("Yay")
