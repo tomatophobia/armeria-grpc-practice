cfdbbf8bcd08fded5f8e1f54eb2acb9b95bbabfd
Youngseo Choi <ys550499@gmail.com>
2023-01-23 23:55:05 +0900

    docService

--- src/main/java/example/armeria/blog/grpc/Main.java
+++ src/main/java/example/armeria/blog/grpc/Main.java
@@ -1,7 +1,10 @@
 package example.armeria.blog.grpc;
 
 import com.linecorp.armeria.server.Server;
+import com.linecorp.armeria.server.docs.DocService;
+import com.linecorp.armeria.server.docs.DocServiceFilter;
 import com.linecorp.armeria.server.grpc.GrpcService;
+import io.grpc.reflection.v1alpha.ServerReflectionGrpc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,14 +12,25 @@ public final class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     static Server newServer(int port) throws Exception {
+        final BlogPost exampleRequest = BlogPost.newBuilder()
+                .setTitle("Example title")
+                .setContent("Example content")
+                .build();
+        final DocService docService = DocService.builder()
+                .exampleRequests(BlogServiceGrpc.SERVICE_NAME, "CreateBlogPost", exampleRequest)
+                .exclude(DocServiceFilter.ofServiceName(ServerReflectionGrpc.SERVICE_NAME))
+                .build();
+
         final GrpcService grpcService = GrpcService.builder()
                 .addService(new BlogService())
+                .enableUnframedRequests(true)
                 .exceptionMapping(new GrpcExceptionHandler())
                 .useBlockingTaskExecutor(true)
                 .build();
         return Server.builder()
                 .http(port)
                 .service(grpcService)
+                .serviceUnder("/docs", docService)
                 .build();
     }
 
