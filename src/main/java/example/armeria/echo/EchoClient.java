c122d07b59535bbcd349e248073e57bf4b2d31e5
Youngseo Choi <ys550499@gmail.com>
2023-09-25 02:59:12 +0900

    Modify `EchoClient` constructor

--- src/main/java/example/armeria/echo/EchoClient.java
+++ src/main/java/example/armeria/echo/EchoClient.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.linecorp.armeria.client.grpc.GrpcClients;
+import com.linecorp.armeria.common.SessionProtocol;
 
 import example.armeria.echo.EchoGrpc.EchoBlockingStub;
 import example.armeria.echo.EchoGrpc.EchoStub;
@@ -22,11 +23,10 @@ final class EchoClient {
     private static EchoBlockingStub blockingStub;
     private static EchoStub asyncStub;
 
-    EchoClient(int port) {
-        // h1c = HTTP/1 cleartext, h2c = HTTP/2 cleartext
-        blockingStub = GrpcClients.builder("h1c://127.0.0.1:" + port)
+    EchoClient(int port, SessionProtocol protocol) {
+        blockingStub = GrpcClients.builder(protocol.uriText() + "://127.0.0.1:" + port)
                                   .build(EchoBlockingStub.class);
-        asyncStub = GrpcClients.builder("h1c://127.0.0.1:" + port)
+        asyncStub = GrpcClients.builder(protocol.uriText() + "://127.0.0.1:" + port)
                                .build(EchoStub.class);
     }
 
@@ -149,10 +149,11 @@ final class EchoClient {
     public static void main(String[] args) throws InterruptedException {
         final int port = 8080;
 
-        final EchoClient client = new EchoClient(port);
+        final EchoClient client = new EchoClient(port, SessionProtocol.H1C);
         List<Message> messages = new ArrayList<>();
         for (int i = 1; i < 4; i++) {
-            messages.add(Message.newBuilder().setSeq(i).setTitle("title" + i).setContent("content" + i).build());
+            messages.add(Message.newBuilder().setSeq(i).setTitle("title" + i).setContent("content" + i)
+                                .build());
         }
         client.ManyToMany(messages);
     }
